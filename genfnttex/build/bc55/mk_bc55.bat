caf227bc9d4b435608635b5ba921b63b288015a7
tenka <tenka@6809.net>
2017-09-15 01:46:49 +0900

    コンパイル警告修正

--- genfnttex/build/bc55/mk_bc55.bat
+++ genfnttex/build/bc55/mk_bc55.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 set SRCDIR=..\..\src
-bcc32 -I%SRCDIR% -I%SRCDIR%\for_old_compiler\bc %SRCDIR%\main.cpp %SRCDIR%\FontGetter.cpp %SRCDIR%\misc\fks_fname.cpp %SRCDIR%\misc\mbc.c %SRCDIR%\misc\tga_wrt.c
+bcc32 -I%SRCDIR% -I%SRCDIR%\for_old_compiler\bc %SRCDIR%\genfnttex.cpp %SRCDIR%\FontGetter.cpp %SRCDIR%\misc\fks_fname.cpp %SRCDIR%\misc\mbc.c %SRCDIR%\misc\tga_wrt.c
 endlocal
