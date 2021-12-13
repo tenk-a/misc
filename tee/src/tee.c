b38d4aea14a6dcd72b53fa4a1f480383cd950c18
tenk-a <tenka@6809.net>
2021-12-14 01:04:20 +0900

    tee: -a 追加.

--- tee/src/tee.c
+++ tee/src/tee.c
@@ -6,18 +6,26 @@
 
 int main(int argc, char* argv[])
 {
-	char buf[0x8000] = {0};
 	FILE* fp;
-	if (argc != 2) {
-		fputs("tiny-tee: build " __DATE__ "\n"
-			  "usage> tee outputfile\n"
+	static char buf[0x8000];
+	char const* mode = "wb";
+	char* fname = NULL;
+
+	if (argc < 2) {
+		fputs("tiny-tee: (build:" __DATE__ ")\n"
+			  "usage> tee [-a] outputfile\n"
 			 , stderr
 		);
 		return 1;
 	}
-	fp = fopen(argv[1], "wb");
+	fname = argv[1];
+	if (argc >= 3 && fname[0] == '-' && fname[1] == 'a') {
+		mode = "ab";
+		fname = argv[2];
+	}
+	fp = fopen(fname, mode);
 	if (!fp) {
-		fputs(argv[1], stderr);
+		fputs(fname, stderr);
 		fputs(" : File open error\n", stderr);
 		return 1;
 	}
